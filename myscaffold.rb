5b49287c241ec26803c3a71fd50f63bd7ad7ca6a
anonyme hacker <anonyme201504@gmail.com>
2024-05-21 16:58:01 -0300

    wow

--- myscaffold.rb
+++ myscaffold.rb
@@ -1,20 +1,53 @@
 require "nokogiri"
 require "open-uri"
-@doc=Nokogiri::HTML(URI.open("https://www.nautiljon.com/watashi/prop_amv.php"))
 
-@doc.css("#prop_fiche li a").each do |hey|
-  doc=Nokogiri::HTML(open(hey.attributes["href"].value))
+f=`ls pagehtml`
+g=f.split("\n").select{|g|!g.include?("Identifier")}
+g.each do |code|
+  doc=Nokogiri::HTML(File.read("./pagehtml/"+code))
+
   champs=doc.css("form td [name]")
-  model=hey.text.camelize
-  listchamps=[]
-  champs.each do |champ|
+  model=doc.css("h1").text.camelize
+  model=model.gsub("d'","").gsub("de","").split("Proposition")[1]
+  listchamps={}
+  champs.each_with_index do |champ,i|
     case champ.name
     when "input"
-      listchamps<<champ.attributes["name"].value
+
+      case champ.attributes["type"]
+      when "radio"
+        somename=champ.attributes["name"].value
+        if !listchamps[champ.attributes["name"].value]
+
+          listchamps[champ.attributes["name"].value]={}
+          p "rails g scaffold #{somename.camelize}"
+        end
+        listchamps[champ.attributes["name"].value][champ.attributes["value"].value]=doc.css("[for=#{champ.id}]")[0].text
+        p "insert into #{somename.camelize} (name) values(#{doc.css("[for=#{champ.id}]")[0].text})"
+      when "checkbox"
+        if !listchamps[champ.attributes["name"].value]
+          somename=champ.attributes["name"].value
+          listchamps[champ.attributes["name"].value]={}
+          p "rails g scaffold #{somename.camelize}"
+        end
+        listchamps[champ.attributes["name"].value][champ.attributes["value"].value]=doc.css("[for=#{champ.id}]")[0].text
+        p "insert into #{somename.camelize} (name) values(#{doc.css("[for=#{champ.id}]")[0].text})"
+      else
+        listchamps[i]=champ.attributes["name"].value
+      end
     when "textarea"
-      listchamps<<champ.attributes["name"].value
+      listchamps[i]=champ.attributes["name"].value
     when "select"
-      listchamps<<champ.attributes["name"].value
+      myname=champ.attributes["name"].value
+      options={}
+      champ.css("option").each do |haha|
+        options[haha.attributes["value"].value]=haha.text
+        p "insert into #{myname.camelize} (name) values(#{haha.text})"
+      rescue
+        p "wow"
+      end
+      listchamps[i]={"#{myname}": options}
+      p "rails g scaffold #{myname.camelize}"
     end
   end
   p listchamps
