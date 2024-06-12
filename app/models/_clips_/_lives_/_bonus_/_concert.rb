10459f83d45b4ff3aab3a24f49889af57e70643e
anonyme hacker <anonyme201504@gmail.com>
2024-06-12 08:51:54 -0300

    azerty

--- app/models/_clips_/_lives_/_bonus_/_concert.rb
+++ app/models/_clips_/_lives_/_bonus_/_concert.rb
@@ -1,3 +1,7 @@
 class Clips::Lives::Bonus::Concert < ApplicationRecord
 self.table_name = "_clips___lives___bonus___concerts"
+belongs_to :concerts_type, foreign_key: "_clips_lives_hbonus_concerts_type_id", class_name:"Clips::Lives::Bonus::ConcertsType"
+def self.clips
+  Clips::Lives::Bonus::Concert.all.includes(:concerts_type).where(concerts_type: {name: "Clip"})
+end
 end
